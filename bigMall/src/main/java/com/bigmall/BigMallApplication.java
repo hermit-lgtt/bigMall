14fa6ffa7364c91b8b03466e5a3c703df47d64aa
LJN-hmall <1041796863@qq.com>
2024-07-09 22:57:02 +0800

    mybatis用法回顾,Redis的使用

---
+++ bigMall/src/main/java/com/bigmall/BigMallApplication.java
@@ -0,0 +1,13 @@
+package com.bigmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigMallApplication.class, args);
+    }
+
+}
---
+++ bigMall/src/test/java/com/bigmall/BigMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
